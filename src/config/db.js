6a554dc7fe5bfb5a2ec1afea0191afce7fd31af0
dikshitapoojari09 <dikshitapoojari09@gmail.com>
2026-09-19 00:33:47 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,17 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  if (mongoose.connection.readyState === 1) {
+    return;
+  }
+
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      maxPoolSize: 10,
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
+      maxIdleTimeMS: 60000
+    });
 
     console.log("MongoDB connected successfully");
   } catch (error) {
