3b621b8deb1ddb1450087d5006f93372262dec3c
dikshitapoojari09 <dikshitapoojari09@gmail.com>
2026-09-19 23:22:40 +0530

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
 import User from "../models/User.js";
+import Project from "../models/Project.js";
+import Task from "../models/Task.js";
 
 // ===============================
 // GET CURRENT USER
@@ -69,6 +71,56 @@ export const updateCurrentUser = async (req, res, next) => {
         name: updatedUser.name,
         email: updatedUser.email
       }
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// ===============================
+// DELETE CURRENT USER
+// ===============================
+export const deleteCurrentUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    // Find all projects owned by the user
+    const projects = await Project.find({
+      owner: user._id
+    }).select("_id");
+
+    const projectIds = projects.map((project) => project._id);
+
+    // Delete tasks belonging to the user's projects
+    await Task.deleteMany({
+      project: { $in: projectIds }
+    });
+
+    // Delete any tasks directly owned by the user
+    await Task.deleteMany({
+      owner: user._id
+    });
+
+    // Delete the user's projects
+    await Project.deleteMany({
+      owner: user._id
+    });
+
+    // Finally delete the user
+    await User.deleteOne({
+      _id: user._id
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User account and related data deleted successfully"
     });
   } catch (error) {
     next(error);
